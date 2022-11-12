57fbc2a4dd0ff5a890df2c29e66067f93efba0f4
Chaerul Umam <chaerulumamb244@gmail.com>
2022-11-12 10:52:30 +0700

    learn Fect Data from API

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {FlatList, Text, TouchableOpacity, View} from 'react-native';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      data: [],
+    };
   }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => this.setState({data: json}, () => console.log(json)))
+      .catch(err => console.log(err));
+  };
 
   render() {
     return (
       <View>
-        <Text>Hellow</Text>
+        <TouchableOpacity onPress={() => this.getData()}>
+          <Text>Klik Get Data</Text>
+        </TouchableOpacity>
+
+        <FlatList
+          data={this.state.data}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({item, index}) => (
+            <View style={{marginTop: 10}}>
+              <Text>{item.title}</Text>
+              <Text>{item.body}</Text>
+            </View>
+          )}
+        />
       </View>
     );
   }
