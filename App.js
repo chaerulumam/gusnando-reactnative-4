6b4d2bed009446685e3cfdea683d1b5e239706bb
Chaerul Umam <chaerulumamb244@gmail.com>
2022-11-12 10:33:24 +0700

    Firts Commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Hellow</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
